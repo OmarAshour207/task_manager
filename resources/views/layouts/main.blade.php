19a829c2e0cf7ab15670c78308756d5d435c729a
unknown <omarzizo207@gmail.com>
2020-10-13 18:18:36 +0200

    remove icon

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -7,8 +7,8 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Taskly') }}</title>
-    <link rel="shortcut icon" href="{{ asset('assets/img/favicon.ico') }}">
+    <title>{{ config('app.name', 'Volcano') }}</title>
+{{--    <link rel="shortcut icon" href="{{ asset('assets/img/favicon.ico') }}">--}}
 
     <!-- General CSS Files -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
